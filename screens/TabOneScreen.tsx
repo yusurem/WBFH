13dba55d1232794ef82cca8800c56070b2d4900b
JaeYeongYoo <dbwodud34@gmail.com>
2022-06-12 13:41:43 +0900

    git init

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
+import ComingBusInfo from '../components/ComingBusInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
@@ -9,7 +9,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <ComingBusInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
